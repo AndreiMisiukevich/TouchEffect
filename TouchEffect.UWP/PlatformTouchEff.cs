0e176d59a0d9be49c3f60ad70243f8349897419d
Piers Deseilligny <42496508+piersdeseilligny@users.noreply.github.com>
2019-11-20 08:29:55 +0000

    Handle PointerCaptureLost correctly on UWP + Add ScrollPage sample page (#30)
    * Fix UWP PointerRelease always Cancelling Touch
    
    * Handle UWP PointerCaptureLost correctly (Issue #29)
    
    * Add ScrollPage sample
    
    * improve ScrollPage.xaml.cs code style
    
    * Fix UWP Pointer Release event
    
    * code style improvements
    
    * [UWP] Fix cancel being called twice by pointer exit and release
    
    * remove _isHoverSupported

--- TouchEffect.UWP/PlatformTouchEff.cs
+++ TouchEffect.UWP/PlatformTouchEff.cs
@@ -22,7 +22,7 @@ namespace TouchEffect.UWP
         private TouchEff _effect;
 
         private bool _pressed;
-		private bool _isHoverSupported;
+        private bool _intentionalCaptureLoss;
 
         protected override void OnAttached()
         {
@@ -37,62 +37,77 @@ namespace TouchEffect.UWP
                 Container.PointerCanceled += OnPointerCanceled;
                 Container.PointerExited += OnPointerExited;
                 Container.PointerEntered += OnPointerEntered;
+                Container.PointerCaptureLost += OnPointerCaptureLost;
             }
         }
 
-		protected override void OnDetached()
-		{
-			_effect.Control = null;
-			_effect = null;
-			if (Container != null)
-			{
-				Container.PointerPressed -= OnPointerPressed;
-				Container.PointerReleased -= OnPointerReleased;
-				Container.PointerCanceled -= OnPointerCanceled;
-				Container.PointerExited -= OnPointerExited;
-				Container.PointerEntered -= OnPointerEntered;
-
-				_pressed = false;
-			}
-		}
-
-		private void OnPointerEntered(object sender, PointerRoutedEventArgs e)
+        protected override void OnDetached()
         {
-			_isHoverSupported = true;
-			if (_pressed)
-			{
-				Element.GetTouchEff().HandleTouch(TouchStatus.Started);
-			}
-			Element.GetTouchEff().HandleHover(HoverStatus.Entered);
-		}
+            _effect.Control = null;
+            _effect = null;
+            if (Container != null)
+            {
+                Container.PointerPressed -= OnPointerPressed;
+                Container.PointerReleased -= OnPointerReleased;
+                Container.PointerCanceled -= OnPointerCanceled;
+                Container.PointerExited -= OnPointerExited;
+                Container.PointerEntered -= OnPointerEntered;
+                Container.PointerCaptureLost -= OnPointerCaptureLost;
+
+                _pressed = false;
+            }
+        }
+
+        private void OnPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (_pressed)
+            {
+                Element.GetTouchEff().HandleTouch(TouchStatus.Started);
+            }
+            Element.GetTouchEff().HandleHover(HoverStatus.Entered);
+        }
 
         private void OnPointerExited(object sender, PointerRoutedEventArgs e)
         {
-			_isHoverSupported = true;
-			if (_pressed)
-			{
-				Element.GetTouchEff().HandleTouch(TouchStatus.Canceled);
-			}
-			Element.GetTouchEff().HandleHover(HoverStatus.Exited);
-		}
+            if (_pressed)
+            {
+                Element.GetTouchEff().HandleTouch(TouchStatus.Canceled);
+            }
+            Element.GetTouchEff().HandleHover(HoverStatus.Exited);
+        }
 
         private void OnPointerCanceled(object sender, PointerRoutedEventArgs e)
         {
             _pressed = false;
             Element.GetTouchEff().HandleTouch(TouchStatus.Canceled);
+            Element.GetTouchEff().HandleHover(HoverStatus.Exited);
+        }
+
+        private void OnPointerCaptureLost(object sender, PointerRoutedEventArgs e)
+        {
+            if (_intentionalCaptureLoss) return;
+            _pressed = false;
+            Element.GetTouchEff().HandleTouch(TouchStatus.Canceled);
+            Element.GetTouchEff().HandleHover(HoverStatus.Exited);
         }
 
         private void OnPointerReleased(object sender, PointerRoutedEventArgs e)
         {
-			_pressed = false;
-			Element.GetTouchEff().HandleTouch(_pressed && (Element.GetTouchEff().HoverStatus == HoverStatus.Entered || !_isHoverSupported) ? TouchStatus.Completed : TouchStatus.Canceled);
+
+            if(_pressed && (Element.GetTouchEff().HoverStatus == HoverStatus.Entered))
+                Element.GetTouchEff().HandleTouch(TouchStatus.Completed);
+            else if(Element.GetTouchEff().HoverStatus != HoverStatus.Exited)
+                Element.GetTouchEff().HandleTouch(TouchStatus.Canceled);
+            _pressed = false;
+            _intentionalCaptureLoss = true;
         }
 
         private void OnPointerPressed(object sender, PointerRoutedEventArgs e)
         {
-			_pressed = true;
-			Container.CapturePointer(e.Pointer);
+            _pressed = true;
+            Container.CapturePointer(e.Pointer);
             Element.GetTouchEff().HandleTouch(TouchStatus.Started);
+            _intentionalCaptureLoss = false;
         }
     }
 }
---
+++ TouchEffectSample/ScrollPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TouchEffectSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScrollPage : ContentPage
+    {
+        public ScrollPage()
+        {
+            InitializeComponent();
+            foreach(BoxView box in boxstack.Children)
+            {
+                TouchEffect.TouchEff touch = new TouchEffect.TouchEff();
+                box.Effects.Add(touch);
+                touch.Completed += Completed;
+                touch.HoverStateChanged += HoverStateChanged;
+                touch.StateChanged += StateChanged;
+            }
+        }
+
+        private void StateChanged(VisualElement sender, TouchEffect.EventArgs.TouchStateChangedEventArgs args)
+        {
+            sender.BackgroundColor = args.State == TouchEffect.Enums.TouchState.Pressed ? Color.Red : Color.Black;
+        }
+
+        private void HoverStateChanged(VisualElement sender, TouchEffect.EventArgs.HoverStateChangedEventArgs args)
+        {
+            sender.Opacity = args.State == TouchEffect.Enums.HoverState.Hovering ? 0.5 : 1;
+        }
+
+        private void Completed(VisualElement sender, TouchEffect.EventArgs.TouchCompletedEventArgs args)
+        {
+            Application.Current.MainPage.DisplayAlert("Clicked on a square", "The Completed event was fired", "Cancel");
+        }
+    }
+}
