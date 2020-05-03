e723471281fb67ce8a9b33513c0ebfe18da595c5
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-05-03 16:58:11 +0300

    hide useless extension methods

--- TouchEffect/Extensions/TouchEffExtensions.cs
+++ TouchEffect/Extensions/TouchEffExtensions.cs
@@ -5,13 +5,10 @@ namespace TouchEffect.Extensions
 {
     public static class TouchEffExtensions
     {
-        public static TouchImage AsTouchImage(this BindableObject bindable)
-            => bindable as TouchImage;
-
         public static TouchEff GetTouchEff(this BindableObject bindable)
             => (bindable as VisualElement)?.Effects.FirstOrDefault(x => x is TouchEff) as TouchEff;
 
-        public static int AdjustDurationMultiplier(this int duration, double? multiplier)
+        internal static int AdjustDurationMultiplier(this int duration, double? multiplier)
             => multiplier.HasValue
                 ? (int)(multiplier.Value * duration)
                 : duration;
