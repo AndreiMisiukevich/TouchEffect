2636c9ebdca32b7b5f96b5d4db340916c3de4086
Yann Zahringer Ferrando <YZahringer@users.noreply.github.com>
2020-06-14 22:37:21 +0200

    Optional TouchEff.Command (#63)
    * CanExecute return true without Command
    
    * cleanup

--- TouchEffect/TouchEff.cs
+++ TouchEffect/TouchEff.cs
@@ -914,7 +914,7 @@ namespace TouchEffect
         public bool CanExecute
             => IsAvailable &&
             Control.IsEnabled &&
-            ((Command?.CanExecute(CommandParameter) ?? false) || Completed != null);
+            (Command?.CanExecute(CommandParameter) ?? true);
 
         [EditorBrowsable(EditorBrowsableState.Never)]
         public VisualElement Control
